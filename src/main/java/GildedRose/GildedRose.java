55b3cdb39fdfebffbf208a5a211ac08d92b76737
Matheus Telles <mwtelles2012@hotmail.com>
2022-04-19 15:47:39 -0300

    Patterns Used: Decompose Conditional, Magic Literal and Replace
    Applying decompose conditional on invocation of items, magic literal on repetitive strings and replace to eliminate unnecessary ifs

--- src/main/java/GildedRose/GildedRose.java
+++ src/main/java/GildedRose/GildedRose.java
@@ -3,58 +3,53 @@ package GildedRose;
 class GildedRose {
     Item[] items;
 
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String CONJURED = "Conjured Mana Cake";
+
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item item : items) {
+            updateItemQuality(item);
+        }
+    }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+    public void countQuality(Item item, int adjust) {
+        int adjustQuality = item.quality + adjust;
+        if (adjustQuality >= 0 && adjustQuality <= 50) {
+            item.quality = adjustQuality;
+        }
+    }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+    public void updateItemQuality(Item item) {
+        int conjuredQuality = item.name.equals(CONJURED) ? -2 : -1;
+        if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE) && !item.name.equals(SULFURAS)) {
+            countQuality(item, conjuredQuality);
+        } else {
+            countQuality(item, 1);
+            if (item.name.equals(BACKSTAGE) && item.sellIn < 11) {
+                countQuality(item, 1);
             }
+        }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+        if (!item.name.equals(SULFURAS)) {
+            item.sellIn = item.sellIn - 1;
+        }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
+        if (item.sellIn < 0) {
+            if (!item.name.equals(AGED_BRIE)) {
+                if (!item.name.equals(BACKSTAGE)) {
+                    if (!item.name.equals(SULFURAS)) {
+                        countQuality(item, conjuredQuality);
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality;
+                        item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+                    countQuality(item, 1);
                 }
             }
         }
